d20cea6104e74931ca7fb03bb6b0541399937d69
vcokltfre <vcokltfre@gmail.com>
2021-04-02 19:34:59 +0100

    feat: add content

---
+++ main.py
@@ -0,0 +1,17 @@
+from os import getenv
+from dotenv import load_dotenv
+
+from src.internal.bot import Bot
+
+
+load_dotenv()
+
+bot = Bot()
+
+bot.load_extensions(
+    "jishaku",
+    "src.cogs.internal.error_handler",
+    "src.cogs.internal.general",
+)
+
+bot.run(getenv("TOKEN"))
